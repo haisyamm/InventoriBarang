b76e83c092a5a12bf95ad487cd0b53bd5f406f42
Ragil Wicaksono <53271616+jujunjuna189@users.noreply.github.com>
2022-08-07 08:43:57 +0700

    Footer

--- resources/views/layouts/app_template.blade.php
+++ resources/views/layouts/app_template.blade.php
@@ -330,12 +330,12 @@
                             <ul class="list-inline list-inline-dots mb-0">
                                 <li class="list-inline-item">
                                     Copyright &copy; 2022
-                                    <a href="." class="link-secondary">Tabler</a>.
+                                    <a href="." class="link-secondary">Inventory</a>.
                                     All rights reserved.
                                 </li>
                                 <li class="list-inline-item">
                                     <a href="./changelog.html" class="link-secondary" rel="noopener">
-                                        v1.0.0-beta9
+                                        v1.0.0
                                     </a>
                                 </li>
                             </ul>
