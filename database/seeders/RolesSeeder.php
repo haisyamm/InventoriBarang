82fc3e7dd77698da51ad428590c4f1136ca7270c
Ragil Wicaksono <53271616+jujunjuna189@users.noreply.github.com>
2022-08-07 07:29:33 +0700

    Update

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -4,9 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\RolesModel;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class RoleSeeder extends Seeder
+class RolesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
