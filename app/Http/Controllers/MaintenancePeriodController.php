144239ade7080bbceb1b41b3a54b62adc6c6d3a1
haisyamm <haisyammaulana22@gmail.com>
2022-08-11 07:23:49 +0700

    Create MaintenancePeriodController.php

---
+++ app/Http/Controllers/MaintenancePeriodController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MaintenancePeriod;
+use App\Models\ProductModel;
+use App\Models\VendorModel;
+use Illuminate\Http\Request;
+
+class MaintenancePeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['period'] = MaintenancePeriod::all();
+        return view('maintenance.period.list', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        $data['product'] = ProductModel::all();
+        $data['vendor'] = VendorModel::all();
+
+        return view('maintenance.period.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MaintenancePeriod  $maintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MaintenancePeriod $maintenancePeriod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MaintenancePeriod  $maintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MaintenancePeriod $maintenancePeriod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MaintenancePeriod  $maintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MaintenancePeriod $maintenancePeriod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MaintenancePeriod  $maintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MaintenancePeriod $maintenancePeriod)
+    {
+        //
+    }
+}
