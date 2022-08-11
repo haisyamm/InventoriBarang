2993ed2ce43bb190980ccca4f43f5e1dd63319d7
haisyamm <haisyammaulana22@gmail.com>
2022-08-11 07:23:53 +0700

    Create DetailMaintenancePeriodController.php

---
+++ app/Http/Controllers/DetailMaintenancePeriodController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DetailMaintenancePeriod;
+use Illuminate\Http\Request;
+
+class DetailMaintenancePeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DetailMaintenancePeriod  $detailMaintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DetailMaintenancePeriod $detailMaintenancePeriod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DetailMaintenancePeriod  $detailMaintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DetailMaintenancePeriod $detailMaintenancePeriod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DetailMaintenancePeriod  $detailMaintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DetailMaintenancePeriod $detailMaintenancePeriod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DetailMaintenancePeriod  $detailMaintenancePeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DetailMaintenancePeriod $detailMaintenancePeriod)
+    {
+        //
+    }
+}
