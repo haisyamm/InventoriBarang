aa052f8cb18b11973eb8580c0ace24f7a7f051d3
haisyamm <haisyammaulana22@gmail.com>
2022-08-12 06:00:51 +0700

    Update assets.blade.php

--- resources/views/assets/assets.blade.php
+++ resources/views/assets/assets.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-header d-flex justify-content-between">
-                <h3 class="card-title">Asset Management</h3>
+                <h3 class="card-title">Assets Management</h3>
                 <div class="">
                     <a href="#" class="btn btn-dark">Action</a>
                 </div>
@@ -30,45 +30,91 @@
                         <thead>
                             <tr>
                                 <th class="w-1"><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select all invoices"></th>
-                                <th class="w-1">Code Item
+                                <th class="text-center" style="width: 50px;">
+                                    <span class="text-muted">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <circle cx="12" cy="12" r="2"></circle>
+                                            <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
+                                        </svg>
+                                    </span>
+                                </th>
+                                <th class="w-1">Asset Id
                                     <!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-dark icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                         <polyline points="6 15 12 9 18 15"></polyline>
                                     </svg>
                                 </th>
-                                <th>Name Item</th>
-                                <th>Brand</th>
-                                <th>Uom</th>
-                                <th>Purchase Price</th>
-                                <th>Selling Price</th>
-                                <th>Stock</th>
-                                <th></th>
+                                <th>Asset Name</th>
+                                <th>Asset Description</th>
+                                <th>Asset Category</th>
+                                <th>Asset Type</th>
+                                <th>Asset Status</th>
+                                <th class="text-center" style="width: 50px;">
+                                    <span class="text-muted">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z"></path>
+                                            <path d="M16 5l3 3"></path>
+                                            <path d="M9 7.07a7.002 7.002 0 0 0 1 13.93a7.002 7.002 0 0 0 6.929 -5.999"></path>
+                                        </svg>
+                                    </span>
+                                </th>
+                                <th class="text-center" style="width: 50px;">
+                                    <span class="text-muted">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <line x1="4" y1="7" x2="20" y2="7"></line>
+                                            <line x1="10" y1="11" x2="10" y2="17"></line>
+                                            <line x1="14" y1="11" x2="14" y2="17"></line>
+                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                        </svg>
+                                    </span>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($product as $val)
+                            @foreach($assets as $val)
                             <tr>
                                 <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-                                <td>{{ $val->code }}</td>
+                                <td class="text-center">
+                                    <a href="#" class="btn btn-icon border-dashed bg-dark-lt">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <circle cx="12" cy="12" r="2"></circle>
+                                            <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
+                                        </svg>
+                                    </a>
+                                </td>
+                                <td>ABC000{{ $val->id }}</td>
                                 <td>{{ $val->name }}</td>
-                                <td>{{ $val->brand }}</td>
-                                <td>{{ $val->uom }}</td>
-                                <td>{{ App\Models\GlobalModel::currency_format($val->purchase_price) }}</td>
-                                <td>{{ App\Models\GlobalModel::currency_format($val->selling_price) }}</td>
-                                <td>{{ $val->stock->stock }}</td>
-                                <td class="text-end">
-                                    <span class="dropdown">
-                                        <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                                        <div class="dropdown-menu dropdown-menu-end">
-                                            <a class="dropdown-item" href="#">
-                                                Action
-                                            </a>
-                                            <a class="dropdown-item" href="#">
-                                                Another action
-                                            </a>
-                                        </div>
-                                    </span>
+                                <td>{{ $val->description }}</td>
+                                <td>{{ $val->category }}</td>
+                                <td>{{ $val->type }}</td>
+                                <td>{{ $val->status }}</td>
+                                <td class="text-center">
+                                    <a href="#" class="btn btn-icon border-dashed bg-yellow-lt">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z"></path>
+                                            <path d="M16 5l3 3"></path>
+                                            <path d="M9 7.07a7.002 7.002 0 0 0 1 13.93a7.002 7.002 0 0 0 6.929 -5.999"></path>
+                                        </svg>
+                                    </a>
+                                </td>
+                                <td class="text-center">
+                                    <a href="#" class="btn btn-icon border-dashed bg-red-lt">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <line x1="4" y1="7" x2="20" y2="7"></line>
+                                            <line x1="10" y1="11" x2="10" y2="17"></line>
+                                            <line x1="14" y1="11" x2="14" y2="17"></line>
+                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                        </svg>
+                                    </a>
                                 </td>
                             </tr>
                             @endforeach
